94574635bec45854eb2890d30586cdc29bd3d6fc
Anastasiya Chultsova <nchulczova@gmail.com>
2022-09-20 16:41:20 +0300

    feat: add validation and styles to signin form

--- src/shared/components/SignIn/Form.tsx
+++ src/shared/components/SignIn/Form.tsx
@@ -3,6 +3,7 @@ import { useFormik } from 'formik'
 import * as yup from 'yup'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
+import { DialogTitle, Link, Typography } from '@mui/material'
 
 type Props = {
     handleClose: () => void
@@ -32,7 +33,15 @@ const Form: FC<Props> = ({handleClose }) => {
   })
   return (
     <div>
-      <form onSubmit={formik.handleSubmit}>
+      <DialogTitle> Sign In</DialogTitle>
+      <form style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingInline: '32px',
+        padding: '16px',
+      }} onSubmit={formik.handleSubmit}>
         <TextField
           fullWidth
           id="email"
@@ -42,6 +51,7 @@ const Form: FC<Props> = ({handleClose }) => {
           onChange={formik.handleChange}
           error={formik.touched.email && Boolean(formik.errors.email)}
           helperText={formik.touched.email && formik.errors.email}
+          sx={{paddingBottom: '16px'}}
         />
         <TextField
           fullWidth
@@ -53,13 +63,22 @@ const Form: FC<Props> = ({handleClose }) => {
           onChange={formik.handleChange}
           error={formik.touched.password && Boolean(formik.errors.password)}
           helperText={formik.touched.password && formik.errors.password}
+          sx={{paddingBottom: '16px'}}
         />
-        <Button onClick={handleClose}>
+        <Typography sx={{paddingBottom: '16px'}}>
+        Don&lsquo;t have an account yet?{' '}
+          <Link>
+								Sign Up
+          </Link>
+        </Typography>
+        <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+          <Button variant="outlined" onClick={handleClose}>
             Cancel
-        </Button>
-        <Button color="primary" variant="contained" fullWidth type="submit">
+          </Button>
+          <Button color="primary" type="submit" variant="contained">
           Submit
-        </Button>
+          </Button>
+        </div>
       </form>
     </div>
   )
