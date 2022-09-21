24968c91ee22b20c7dfd635c3cfd58ed4dff3277
Anastasiya Chultsova <nchulczova@gmail.com>
2022-09-21 18:31:24 +0300

    fix: validation

--- src/shared/components/SignIn/Form.tsx
+++ src/shared/components/SignIn/Form.tsx
@@ -10,21 +10,39 @@ type Props = {
 }
 
 const validationSchema = yup.object({
+  name: yup
+    .string()
+    .max(30, 'Name length must be between 1 and 30 symbols')
+    .matches(
+      /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
+      'Name can contain only letters')
+    .required('Please enter your name'),
+  surname: yup
+    .string()
+    .max(30, 'Surname length must be between 1 and 30 symbols')
+    .matches(
+      /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
+      'Surname can contain only letters')
+    .test('surname-not-name', 'Surname can\'t be the same as name', function(value){
+      return this.parent.name !== value
+    })
+    .required('Please enter your surname'),
   email: yup
     .string()
-    .email('Enter a valid email')
-    .required('Email is required'),
+    .max(256, 'Email length must be no more than 256 symbols')
+    .email('Please provide valid email address ')
+    .required('Please enter your email'),
   password: yup
     .string()
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
-  repeatPassword: yup
+    .max(64, 'Password lenght must be from 8 to 64 symbols')
+    .min(8, 'Password lenght must be from 8 to 64 symbols')
+    .required('Please enter your password'),
+  confirmPassword: yup
     .string()
-    .label('confirm password')
-    .test('passwords-match', 'Passwords must match', function(value){
+    .test('passwords-match', 'Passwords don\'t match', function(value){
       return this.parent.password === value
     })
-    .required()
+    .required('Please confirm your password')
 })
 
 const Form: FC<Props> = ({handleClose }) => {
@@ -32,9 +50,11 @@ const Form: FC<Props> = ({handleClose }) => {
   const [authError, setAuthError] = useState(null)
   const formik = useFormik({
     initialValues: {
+      name: '',
+      surname: '',
       email: '',
       password: '',
-      repeatPassword: ''
+      confirmPassword: ''
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -61,20 +81,42 @@ const Form: FC<Props> = ({handleClose }) => {
         alignItems: 'center',
         padding: '16px',
       }} onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-          sx={{paddingBottom: '16px'}}
-        />
         {loginSignup === 'signup'
           ? (
             <Box>
+              <TextField
+                fullWidth
+                id="name"
+                name="name"
+                label="Name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+                helperText={formik.touched.name && formik.errors.name}
+                sx={{paddingBottom: '16px'}}
+              />
+              <TextField
+                fullWidth
+                id="surname"
+                name="surname"
+                label="Surname"
+                value={formik.values.surname}
+                onChange={formik.handleChange}
+                error={formik.touched.surname && Boolean(formik.errors.surname)}
+                helperText={formik.touched.surname && formik.errors.surname}
+                sx={{paddingBottom: '16px'}}
+              />
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                sx={{paddingBottom: '16px'}}
+              />
               <TextField
                 fullWidth
                 id="password"
@@ -89,29 +131,42 @@ const Form: FC<Props> = ({handleClose }) => {
               />
               <TextField
                 fullWidth
-                id="repeatPassword"
-                name="repeatPassword"
-                label="Repeat password"
-                type="repeatPassword"
-                value={formik.values.repeatPassword}
+                id="confirmPassword"
+                name="confirmPassword"
+                label="Confirm password"
+                type="password"
+                value={formik.values.confirmPassword}
                 onChange={formik.handleChange}
-                error={formik.touched.repeatPassword && Boolean(formik.errors.repeatPassword)}
-                helperText={formik.touched.repeatPassword && formik.errors.repeatPassword}
+                error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
               />
             </Box>
           )
           : (
-            <TextField
-              fullWidth
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
+            <Box>
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                sx={{paddingBottom: '16px'}}
+              />
+              <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </Box>
           )}
         <Box py={3}>
           <Typography>
