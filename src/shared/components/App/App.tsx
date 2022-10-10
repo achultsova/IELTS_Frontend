a98503bcbb1676703ad0bc8b1dbc5f8b556cfd77
achultsova <nchulczova@gmail.com>
2022-10-10 15:24:21 +0200

    feat: add 500 page

--- src/shared/components/App/App.tsx
+++ src/shared/components/App/App.tsx
@@ -4,6 +4,7 @@ import Footer from '../Footer'
 import Header from '../Header'
 import Home from '../Home'
 import PageNotFound from '../PageNotFound'
+import InternalServer from '../InternalServer'
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path="*" element={<PageNotFound/>} />
+          <Route path='/500' element={<InternalServer/>}/>
         </Routes>
       </BrowserRouter>
       <Footer />
