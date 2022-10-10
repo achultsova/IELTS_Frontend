905fce7979eb8a4b2c88322bc93a41d9fd41543b
achultsova <nchulczova@gmail.com>
2022-10-10 14:57:46 +0200

    feat: add 404 page

--- src/shared/components/App/App.tsx
+++ src/shared/components/App/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import {  Route, Routes, BrowserRouter } from 'react-router-dom'
 import Footer from '../Footer'
 import Header from '../Header'
+import Home from '../Home'
+import PageNotFound from '../PageNotFound'
 
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path="*" element={<PageNotFound/>} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   )
---
+++ src/shared/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import { Box } from '@mui/material'
+
+const Home: FC = () => {
+  return (
+    <Box>
+      Home Page
+    </Box>
+  )
+}
+
+export default Home
